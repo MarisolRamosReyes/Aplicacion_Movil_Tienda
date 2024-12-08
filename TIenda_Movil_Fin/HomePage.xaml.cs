b738635e3c1a2509feed1768ce093e1de0e27509
MarisolRamos <I20050036@monclova.tecnm.mx>
2024-12-07 21:12:54 -0600

    pantalla-home

--- TIenda_Movil_Fin/HomePage.xaml.cs
+++ TIenda_Movil_Fin/HomePage.xaml.cs
@@ -2,8 +2,27 @@ namespace TIenda_Movil_Fin;
 
 public partial class HomePage : ContentPage
 {
-	public HomePage()
-	{
-		InitializeComponent();
-	}
+    public HomePage()
+    {
+        InitializeComponent();
+    }
+
+    private async void OnContactPageButtonClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new ContactPage());
+    }
+
+    private async void OnAboutPageButtonClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new AboutPage());
+    }
+    private async void OnLogoutButtonClicked(object sender, EventArgs e)
+    { 
+        bool confirm = await DisplayAlert("Cerrar Sesión", "¿Estás seguro de que deseas cerrar la sesión?", "Sí", "No");
+        if (confirm)
+        {
+            await Navigation.PopToRootAsync();
+        }
+    }
+
 }
